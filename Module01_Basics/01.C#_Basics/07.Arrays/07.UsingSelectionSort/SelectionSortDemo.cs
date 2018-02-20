f47ea8a319ee8b9e6689cf88d68badc9f17dc02a
TanyaRan <tatyanaran@gmail.com>
2018-02-20 23:26:35 +0200

    Arrays

---
+++ Module01_Basics/01.C#_Basics/07.Arrays/07.UsingSelectionSort/SelectionSortDemo.cs
@@ -0,0 +1,51 @@
+﻿namespace UsingSelectionSort
+{
+    using System;
+
+    public class SelectionSortDemo
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] sortedArray = SelectionSortAlgo(arr);
+
+            foreach (var x in arr)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        private static int[] SelectionSortAlgo(int[] arr)
+        {
+            // "Selection sort" algorithm:
+            //   Find the smallest element, swap it with element at the first position, 
+            // find the smallest from the rest, move it at the second position, etc.
+
+            for (int sortedSize = 0; sortedSize < arr.Length; sortedSize++)
+            {
+                int minIndex = sortedSize;
+
+                for (int j = sortedSize + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[sortedSize];
+                arr[sortedSize] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Module01_Basics/01.C#_Basics/07.Arrays/08.MaximalSum/MaxSumOfSequence.cs
@@ -0,0 +1,43 @@
+﻿namespace MaximalSum
+{
+    using System;
+
+    public class MaxSumOfSequence
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxSum = FindMaxSum(arr);
+
+            Console.WriteLine(maxSum);
+        }
+
+        private static int FindMaxSum(int[] arr)
+        {
+            int bestSum = int.MinValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentSum = 0;
+
+                for (int j = i; j < arr.Length; j++)
+                {
+                    currentSum += arr[j];
+                    if (currentSum > bestSum)
+                    {
+                        bestSum = currentSum;
+                    }
+                }
+            }
+
+            return bestSum;
+        }
+    }
+}
